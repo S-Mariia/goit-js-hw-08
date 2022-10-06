7953802ae0fdf3055ecf322517dbf2f78d8d11c5
S-Mariia <105200661+S-Mariia@users.noreply.github.com>
2022-10-07 00:11:06 +0300

    task 1 and task 2 are done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+
+function createGalleryMarkup(list) {
+    return list.reduce((acc, {preview, original, description})=>acc+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" loading="lazy" alt="${description}" />
+</a> `,'')
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
+const CURRENTTIME = "videoplayer-current-time";
+
+const player = new Player('vimeo-player');
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate({seconds}) {
+    localStorage.setItem(CURRENTTIME, seconds);
+}
+
+const currentTime = localStorage.getItem(CURRENTTIME) === null ? 0 : localStorage.getItem(CURRENTTIME);
+
+player.setCurrentTime(currentTime);
 
