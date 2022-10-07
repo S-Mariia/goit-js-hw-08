4975b03980316128a4864277564bc48d45492605
S-Mariia <105200661+S-Mariia@users.noreply.github.com>
2022-10-07 14:23:47 +0300

    did task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+var throttle = require('lodash.throttle');
+
+const RESPOND = "feedback-form-state";
+const feedbackObj = {};
+
+const form = document.querySelector(".feedback-form");
+
+if (localStorage.getItem(RESPOND) !== null) {
+    const feedbackData = JSON.parse(localStorage.getItem(RESPOND));
+    
+    form.elements.email.value = feedbackData.email;
+    form.elements.message.value = feedbackData.message;
+}
+
+form.addEventListener('input', throttle(onInputUpdateLocalStorage,500));
+form.addEventListener("submit", onFormSubmit);
+
+function onInputUpdateLocalStorage(evt) {
+    const { email, message } = evt.currentTarget.elements;
+    feedbackObj.email = email.value;
+    feedbackObj.message = message.value;
+
+    localStorage.setItem(RESPOND, JSON.stringify(feedbackObj));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(feedbackObj);
+
+    localStorage.removeItem(RESPOND);
+    evt.currentTarget.reset();
+}
+
 
